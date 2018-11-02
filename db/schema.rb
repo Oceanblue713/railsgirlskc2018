90b3d7f62e10c3e0a99ce6ba39a0abc9fc165e92
Anri Liy <anri@realquantum.com>
2018-11-02 11:20:40 -0500

    Change database sqlite3 to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_10_12_132828) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.string "user_name"
     t.text "body"
